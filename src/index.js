470c3a1d02f807354515f7d6fce9879f91d6278a
divine <divinemaina@gmail.com>
2024-04-27 20:44:23 +0200

    implement google map  routing functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { ChakraProvider, theme } from '@chakra-ui/react'
+import React from 'react';
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+  
 );
+
+
 
